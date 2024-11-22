0ea8d6e386f9b40cfdcebaeafcd86b7c1242d41b
Yuri4ek <dinourski@gmail.com>
2024-11-22 21:01:44 +0300

    Изменил меню поздравления

--- Space_man/Congratulation.py
+++ Space_man/Congratulation.py
@@ -20,17 +20,23 @@ class SpaceMan(QMainWindow):
         pixmap = QPixmap("Space_man/Nikolaev.jpg")
         self.image.setPixmap(pixmap)
 
-        text = ("Андриян Григорьевич – звездный сын чувашского народа, \n"
-                "человек, открывший космос многим поколениям. Его жизненный \n"
-                "путь – это умение мечтать, трудолюбиво делать все, чтобы эта \n"
-                "мечта стала реальностью. Для всех нас это яркий пример. Мы \n"
-                "должны помнить, сохранять и передавать из поколения в \n"
-                "поколение все его достижения и подвиги.")
+        text = ("Это приложение создано в честь года космонавтики в Чувашии. \n"
+                "Космонавтом ведь неудобно хранить пароля в бумажном виде, \n"
+                "а мое приложение создано именно для таких условий, \n"
+                "чтобы космонавтам всегда было удобно и безопасно хранить \n"
+                "свои пароли.")
+
         self.congratulation.setText(text)
         self.congratulation.setAlignment(Qt.AlignmentFlag.AlignCenter)
 
         # добавляет иконку приложения
         app.setWindowIcon(QIcon("Presentation files/logo.png"))
+
+        # открытие окна входа
+        self.application_button.clicked.connect(self.my_exit)
+
+    def my_exit(self):
+        sys.exit(app.exec())
 
 
 def except_hook(cls, exception, traceback):
